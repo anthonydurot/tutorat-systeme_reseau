b89632d85ff5c27e9825f0399808a1ecb776f6cc
Commun <antoine.duquenoy@polytech-lille.net>
2018-02-20 08:32:29 +0000

    Ajout des structures et prototypes pour les trames

--- TShirt/broadcast.c
+++ TShirt/broadcast.c
@@ -4,19 +4,35 @@
 
 #include "serial.h"
 #include "analog.h"
+#include "broadcast.h"
 
 #define MAC_SIZE	6
 #define IPV4_SIZE	4
 
+#define IP_SOURCE   "172.26.79.205"
+#define IP_DEST     "172.26.79.204"
+
+#define PORT_SOURCE 4269
+#define PORT_DST    80
+
 int main(void)
 {
+
     init_printf();
-    ad_init(0);
-    int val;
+    uint8_t v_capteurs[4];
+    TrameIP trame;
+    init_trame(&trame);
+   
     while(1) {
-        val = (((ad_sample() * 0.00488) - 0.5) * (-100)); // Test du capteur de temperature
-        printf("Valeur : %d\n",val); 
-    }  
+    
+        for(int i = 0; i <= 3; i++) {
+            ad_init(i);
+            v_capteurs[i] = ad_sample();
+        }
+        
+        forger_trame(&trame, v_capteurs);
+        envoyer_trame(&trame);  
+    }
     
     return 0;
 }
---
+++ TShirt/broadcast.h
@@ -0,0 +1,41 @@
+#pragma pack(1)
+typedef struct trameIP {
+
+    uint8_t version_longueur_entete;
+    uint8_t TOS;
+    uint16_t longueur_totale;
+    uint16_t identificateur;
+    uint16_t flags_offset;
+    uint8_t TTL;
+    uint8_t protocole;
+    uint16_t checksum;
+    uint8_t adr_source[4];
+    uint8_t adr_destination[4];
+    Trame_UDP data;
+
+} TrameIP;
+#pragma pack()
+
+#pragma pack(1)
+typedef struct trameUDP {
+
+    uint16_t port_source;
+    uint16_t port_destination;
+    uint16_t longueur;
+    uint8_t checksum;
+    DataUDP data;
+
+} TrameUDP;
+#pragma pack()
+
+#pragma pack(1)
+typedef struct dataUDP {
+
+    uint8_t id_tshirt;
+    uint8_t accel_x;
+    uint8_t accel_y;
+    uint8_t accel_z;
+    uint8_t temp;
+
+} DataUDP;
+#pragma pack()
